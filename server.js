e04beec603c2344de909933747e95edce29ed33e
Lev <lev.bereza@gmail.com>
2024-11-20 10:32:15 +0200

    Enabled prettier for the project.

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const {Server} = require('socket.io');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('./'));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use(express.static("./"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
-    socket.on('message', (message) => {
-        socket.broadcast.emit('message', message);
-    });
+io.on("connection", (socket) => {
+  console.log("New user connected");
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", message);
+  });
 });
 server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
